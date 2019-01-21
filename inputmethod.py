be873a24c4cbdaca81c0bf757e4112be0602c0be
vaibhavnegi14 <46775738+vaibhavnegi14@users.noreply.github.com>
2019-01-21 16:09:30 +0530

    Create inputmethod.py

---
+++ inputmethod.py
@@ -0,0 +1,6 @@
+#this is a example of taking input from user
+
+name=input("enter your name\n");
+regno=int(input("enter your registration number\n"));
+print("my name is",name);
+print("regno:",regno);
