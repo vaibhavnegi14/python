479b9dab77b4d3608e0f2c944f2784068538fdc4
vaibhavnegi14 <46775738+vaibhavnegi14@users.noreply.github.com>
2019-01-22 16:53:00 +0530

    Create example3.py

---
+++ example3.py
@@ -0,0 +1,8 @@
+n1=int(input("enter 10 rupee coins\n"))
+n2=int(input("enter 5 rupee coins\n"))
+n3=int(input("enter 2 rupee coins\n"))
+n4=int(input("enter 1 rupee coins\n"))
+total=n1*10+n2*5+n3*2+n4*1
+print("  piggy bank")
+print("****************")
+print("total amount is",total,"RS")
