f7a05746b3424602738244d9b3986ad6d08d9cc8
vaibhavnegi14 <46775738+vaibhavnegi14@users.noreply.github.com>
2019-01-22 16:52:10 +0530

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,15 @@
+num1=int(input("enter the number1\n"))
+num2=int(input("enter the number2\n"))
+sum=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+mod=num1%num2
+pow=num1**num2
+
+print("sum is",sum)
+print("sub is",sub)
+print("mult is",mult)
+print("div is",div)
+print("mod is",mod)
+print("pow is",pow)
